0d9112def7e97b58615fc4c090c0fe98da199446
Christian Vogel <vogelchr@vogel.cx>
2018-11-12 18:02:44 +0100

    script to correlate two readings, and create a correction polynomial

--- correlate_sensors/correlate_to_ow.py
+++ correlate_sensors/correlate_to_ow.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 
+import argparse
+
 import influxdb
 import matplotlib
 import numpy as np
 
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+
+def linspace_min_max_grow(ndarr, grow):
+    x1 = np.amin(ndarr)
+    x2 = np.amax(ndarr)
+    w = x2 - x1
+    x1 -= grow * w
+    x2 += grow * w
+    return np.linspace(x1, x2, 100)
 
 
 def query_to_np(clt, query):
@@ -25,48 +36,68 @@ def query_to_np(clt, query):
     return t, y
 
 
-clt = influxdb.DataFrameClient(host='localhost', port=8086)
-clt.switch_database('heating')
+parser = argparse.ArgumentParser()
+parser.add_argument('--start', metavar='TIMESTAMP', help='Start time.')
+parser.add_argument('--end', metavar='TIMESTAMP', help='End time.')
+parser.add_argument('-d', '--influx-db', default='heating', metavar='DB',
+                    help='Influx database [heating]')
+parser.add_argument('-i', '--influx-host', default='127.0.0.1', metavar='HOST',
+                    help='Influx host [127.0.0.1]')
+parser.add_argument('-u', '--unit', dest='unit', metavar='UNIT', default='arb',
+                    help='Unit of measurement, e.g. volt, degC, ... [def: arb]')
+parser.add_argument(metavar='measurement1:column1', dest='mc1')
+parser.add_argument(metavar='measurement2:column2', dest='mc2')
 
-t_opto, degC_opto = query_to_np(clt, 'SELECT t_supply_m2 FROM optolink')
-t_opto_rel = (t_opto - t_opto[0]).astype('f') / 1e9
+args = parser.parse_args()
 
-t_min_str = str(np.amin(t_opto)) + 'Z'
-t_max_str = str(np.amax(t_opto)) + 'Z'
+clt = influxdb.DataFrameClient(host=args.influx_host, port=8086)
+clt.switch_database(args.influx_db)
 
+m1, col1 = args.mc1.split(':')
+m2, col2 = args.mc2.split(':')
 
-t_ow, degC_ow = query_to_np(clt, f"SELECT og_sup FROM heating WHERE time >= '{t_min_str}' AND time <= '{t_max_str}'")
-t_ow_rel = (t_ow - t_opto[0]).astype('f') / 1e9
+query = f"SELECT {col1} FROM {m1}"
+if args.start:
+    query += f" WHERE time >= '{args.start}'"
+if args.end:
+    if args.start:
+        query += f" AND time <= '{args.end}'"
+    else:
+        query += f" WHERE time <= '{args.end}'"
 
-print('Minimum timestamp:', np.amin(t_opto), np.amin(t_ow))
-print('Maximum timestamp:', np.amax(t_opto), np.amax(t_ow))
+t1, val1 = query_to_np(clt, query)
+t1_rel = (t1 - t1[0]).astype('f') / 1e9
 
-degC_ow_on_t_opto = np.interp(t_opto_rel, t_ow_rel, degC_ow)
-poly = np.polyfit(degC_opto, degC_ow_on_t_opto, 1)
+t_min_str = str(np.amin(t1)) + 'Z'
+t_max_str = str(np.amax(t1)) + 'Z'
+
+t2, val2 = query_to_np(clt, f"SELECT {col2} FROM {m2} WHERE time >= '{t_min_str}' AND time <= '{t_max_str}'")
+t2_rel = (t2 - t1[0]).astype('f') / 1e9
+
+print('Minimum timestamp:', np.amin(t1), np.amin(t2))
+print('Maximum timestamp:', np.amax(t1), np.amax(t2))
+
+val2_on_t1 = np.interp(t1_rel, t2_rel, val2)
+poly = np.polyfit(val1, val2_on_t1, 1)
 print('Polyfit:', poly)
 
-degC_x_min = np.amin(degC_opto)
-degC_x_max = np.amax(degC_opto)
-degC_x_span = degC_x_max - degC_x_min
-degC_x_min -= 0.1 * degC_x_span
-degC_x_max += 0.1 * degC_x_span
-
-degC_x_lin = np.linspace(degC_x_min, degC_x_max, 100)
-degC_y_lin = np.polyval(poly, degC_x_lin)
+v1_lin = linspace_min_max_grow(val1, 0.1)
+v2_fit = np.polyval(poly, v1_lin)
 
 fig, ax = plt.subplots()
 ax.set_xlabel('Time [s]')
 ax.set_ylabel('Temp [degC]')
-ax.plot(t_opto_rel, degC_opto, label='Opto')
-ax.plot(t_ow_rel, degC_ow, label='Onewire')
+ax.plot(t1_rel, val1, label=col1)
+ax.plot(t2_rel, val2, label=col2)
 ax.legend()
 fig.savefig('time_series.png')
 
 fig, ax = plt.subplots()
-ax.set_xlabel('Temp Optolink [degC]')
-ax.set_ylabel('Temp Onewire [degC]')
-ax.plot(degC_opto, degC_ow_on_t_opto, '*', label='Fit')
-ax.plot(degC_x_lin, degC_y_lin, '-', label='Fit')
+ax.set_xlabel(f'{col1} [{args.unit}]')
+ax.set_ylabel(f'{col2} [{args.unit}]')
+ax.plot(val1, val2_on_t1, '*', label='Fit')
+ax.plot(v1_lin, v2_fit, '-', label='Fit')
 ax.legend()
 fig.savefig('t_vs_t.png')
+
 
